435e8a828da592749e70d0eb4578c3061d58c7bb
Tushar Chavan <tchavan@vmware.com>
2022-08-17 12:35:24 +0100

    strconv and reflect

--- firstHelloWorld/hello-world.go
+++ firstHelloWorld/hello-world.go
@@ -1,12 +1,25 @@
 package main
 
-import "fmt"
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
 
 var (
 	myFirstName, myLastName = "Tushar", "Chavan"
 	myAge                   = calcAge(1990)
+	years                   = "10"
+	ageAfterYears           = calcAgeAfterYears(myAge, years)
 )
+
+func calcAgeAfterYears(ageToday int, years string) int {
+	yearsInt, err := strconv.Atoi(years)
+	if err == nil {
+		return yearsInt + ageToday
+	}
+	return 0
+}
 
 func calcAge(birthYear int) int {
 	return 2022 - birthYear
@@ -16,4 +29,5 @@ func calcAge(birthYear int) int {
 func main() {
 	fmt.Println("Hello World, my First Name is", myFirstName, ",my last name is", myLastName, ". I am ", myAge, " years old.")
 	fmt.Println("Type of firstname is ", reflect.TypeOf(myFirstName), ". Type of age is ", reflect.TypeOf(myAge))
+	fmt.Println("I will be", ageAfterYears, "years old after", years, "years.")
 }
