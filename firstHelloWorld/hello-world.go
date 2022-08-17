c7310c5ef0507aa0a562b418b6a83feeb6f048db
Tushar Chavan <tchavan@vmware.com>
2022-08-17 16:08:48 +0100

    variables

--- firstHelloWorld/hello-world.go
+++ firstHelloWorld/hello-world.go
@@ -27,7 +27,19 @@ func calcAge(birthYear int) int {
 }
 
 func main() {
+	myFavSport := "Cricket"
+
+	//Declaring myFavSportPtr as a pointer
+	//&myFavSport will return the memory address of myFavSport
+	//we are storing that memory address in a pointer variable called myFavSportPtr
+	var myFavSportPtr *string = &myFavSport
+
 	fmt.Println("Hello World, my First Name is", myFirstName, ",my last name is", myLastName, ". I am ", myAge, " years old.")
 	fmt.Println("Type of firstname is ", reflect.TypeOf(myFirstName), ". Type of age is ", reflect.TypeOf(myAge))
 	fmt.Println("I will be", ageAfterYears, "years old after", years, "years.")
+	fmt.Println("memory address of myFavSport is ", &myFavSport)
+	//we have stored memory address of myFavSport variable in myFavSportPtr variable.
+	//*myFavSportPtr will return the value stored in memory address at which the pointer myFavSportPtr is pointing
+	fmt.Println("Memory address of myFavSport is", myFavSportPtr, "and value stored is ", *myFavSportPtr)
+
 }
